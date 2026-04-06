e8f337cb9028a366e707db385ba355777ffe29d1
Sharon <165468813+CodeWithSharon@users.noreply.github.com>
2026-04-06 16:01:56 +0530

    Add comment functionality to PostRegistry contract

--- contracts/PostRegistry.sol
+++ contracts/PostRegistry.sol
@@ -20,7 +20,19 @@ contract PostRegistry {
         bool exists;
     }
 
+    struct Comment {
+        uint256 id;
+        uint256 postId;
+        address author;
+        string ipfsHash;
+        bytes32 contentHash;
+        uint256 timestamp;
+        uint256 likeCount;
+        bool exists;
+    }
+
     uint256 public postCount;
+    uint256 public commentCount;
 
     mapping(uint256 => Post) private posts;
     mapping(address => uint256[]) private userPosts;
@@ -29,6 +41,12 @@ contract PostRegistry {
     mapping(uint256 => uint256) private totalVoteWeight;
     mapping(uint256 => mapping(address => bool)) private hasLiked;
     mapping(uint256 => mapping(address => bool)) private hasFlagged;
+    mapping(uint256 => Comment) private comments;
+    mapping(uint256 => uint256[]) private postComments;
+    mapping(uint256 => mapping(address => bool)) private hasLikedComment;
+    mapping(uint256 => address) private originalAuthors;
+    mapping(uint256 => uint256) private repostOf;
+    mapping(uint256 => uint256) private repostCount;
 
     event PostCreated(uint256 indexed postId, address indexed author, string ipfsHash, uint256 timestamp);
     event PostLiked(uint256 indexed postId, address indexed liker, address indexed author);
@@ -36,6 +54,9 @@ contract PostRegistry {
     event PostHidden(uint256 indexed postId);
     event TipSent(uint256 indexed postId, address indexed from, address indexed to, uint256 amount);
     event Voted(uint256 indexed postId, address indexed voter, bool hideVote);
+    event CommentCreated(uint256 indexed commentId, uint256 indexed postId, address indexed author);
+    event CommentLiked(uint256 indexed commentId, address indexed liker, address indexed author);
+    event PostReposted(uint256 indexed newPostId, uint256 indexed originalPostId, address indexed reposter);
 
     constructor(address _contentTokenAddress) {
         contentToken = ContentToken(_contentTokenAddress);
@@ -109,6 +130,67 @@ contract PostRegistry {
         emit TipSent(_postId, msg.sender, posts[_postId].author, msg.value);
     }
 
+    function deletePost(uint256 _postId) external {
+        require(posts[_postId].exists, "Post does not exist");
+        require(posts[_postId].author == msg.sender, "Only author can delete");
+        posts[_postId].isHidden = true;
+        emit PostHidden(_postId);
+    }
+
+    function repost(uint256 _originalPostId) external {
+        require(posts[_originalPostId].exists, "Post does not exist");
+        require(!posts[_originalPostId].isHidden, "Post is hidden");
+        require(posts[_originalPostId].author != msg.sender, "Cannot repost your own post");
+        postCount++;
+        posts[postCount] = Post({
+            id: postCount,
+            author: msg.sender,
+            ipfsHash: posts[_originalPostId].ipfsHash,
+            contentHash: posts[_originalPostId].contentHash,
+            timestamp: block.timestamp,
+            likeCount: 0,
+            flagCount: 0,
+            tipAmount: 0,
+            isHidden: false,
+            exists: true
+        });
+        originalAuthors[postCount] = posts[_originalPostId].author;
+        repostOf[postCount] = _originalPostId;
+        repostCount[_originalPostId]++;
+        userPosts[msg.sender].push(postCount);
+        contentToken.mint(posts[_originalPostId].author, 1 * 10 ** 18);
+        emit PostReposted(postCount, _originalPostId, msg.sender);
+    }
+
+    function addComment(uint256 _postId, string memory _ipfsHash, bytes32 _contentHash) external {
+        require(posts[_postId].exists, "Post does not exist");
+        require(!posts[_postId].isHidden, "Post is hidden");
+        require(bytes(_ipfsHash).length > 0, "IPFS hash cannot be empty");
+        commentCount++;
+        comments[commentCount] = Comment({
+            id: commentCount,
+            postId: _postId,
+            author: msg.sender,
+            ipfsHash: _ipfsHash,
+            contentHash: _contentHash,
+            timestamp: block.timestamp,
+            likeCount: 0,
+            exists: true
+        });
+        postComments[_postId].push(commentCount);
+        emit CommentCreated(commentCount, _postId, msg.sender);
+    }
+
+    function likeComment(uint256 _commentId) external {
+        require(comments[_commentId].exists, "Comment does not exist");
+        require(!hasLikedComment[_commentId][msg.sender], "Already liked");
+        require(comments[_commentId].author != msg.sender, "Cannot like your own comment");
+        hasLikedComment[_commentId][msg.sender] = true;
+        comments[_commentId].likeCount++;
+        contentToken.mint(comments[_commentId].author, 1 * 10 ** 18);
+        emit CommentLiked(_commentId, msg.sender, comments[_commentId].author);
+    }
+
     function verifyPost(uint256 _postId, bytes32 _contentHash) external view returns (bool) {
         require(posts[_postId].exists, "Post does not exist");
         return posts[_postId].contentHash == _contentHash;
@@ -134,6 +216,40 @@ contract PostRegistry {
             result[i] = posts[ids[i]];
         }
         return result;
+    }
+
+    function getComment(uint256 _commentId) external view returns (Comment memory) {
+        require(comments[_commentId].exists, "Comment does not exist");
+        return comments[_commentId];
+    }
+
+    function getPostComments(uint256 _postId) external view returns (Comment[] memory) {
+        uint256[] memory ids = postComments[_postId];
+        Comment[] memory result = new Comment[](ids.length);
+        for (uint256 i = 0; i < ids.length; i++) {
+            result[i] = comments[ids[i]];
+        }
+        return result;
+    }
+
+    function getCommentCount(uint256 _postId) external view returns (uint256) {
+        return postComments[_postId].length;
+    }
+
+    function getRepostCount(uint256 _postId) external view returns (uint256) {
+        return repostCount[_postId];
+    }
+
+    function getOriginalAuthor(uint256 _postId) external view returns (address) {
+        return originalAuthors[_postId];
+    }
+
+    function isRepost(uint256 _postId) external view returns (bool) {
+        return repostOf[_postId] != 0;
+    }
+
+    function getOriginalPostId(uint256 _postId) external view returns (uint256) {
+        return repostOf[_postId];
     }
 
     function checkLiked(uint256 _postId, address _user) external view returns (bool) {
